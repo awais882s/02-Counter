fb34947c64273c6e8cdb8dd15ecdb381d0ba1ad0
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-11-24 17:49:25 +0500

    increment colors

--- counter.js
+++ counter.js
@@ -5,12 +5,20 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-  btn.addEventListener("click", function(e) {
+  btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if (styles.contains("dec")){
-      count--; 
+    if (styles.contains("dec")) {
+      count--;
+    } else if (styles.contains("inc")) {
+      count++;
+    }
+    else {
+      count  = 0;
+    }
+    if (count > 0) {
+      value.style.color = "Green"
+      
     }
     value.textContent = count;
-    
   });
 });
